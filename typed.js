1ba28738ccd633676ac2f55b7e2c9ee7279cc370
slaxfrmdao <alexgogu315@gmail.com>
2024-08-15 22:39:03 +0300

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'quit Com','LIL SKID','connecting :O','Fuck Pedos','Sim Swapped :O','Yall Talk Too Much Shit',':D'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
